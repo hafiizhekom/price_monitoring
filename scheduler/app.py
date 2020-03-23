9b441ec0a9f34ad433f022eb29c101c2933ea744
hafiizhekom <hafiizhekom@yahoo.com>
2020-03-24 04:57:15 +0700

    Heroku:Add Scheduler

--- scheduler/app.py
+++ scheduler/app.py
@@ -5,11 +5,12 @@ from data.app import Data
 class Scheduler():
   def schedule(self):
     data = Data('../data/db/db.json')
+    print('This job is run every hours.')
     for url in data.getUrl():
       data.updatePrice(url, data.crawlPrice(url))
 
 if __name__ == '__main__':
   scheduler = BlockingScheduler()
-  scheduler.add_job(Scheduler().schedule, 'interval', hours=1)
+  scheduler.add_job(Scheduler().schedule, 'interval', minutes=1)
   scheduler.start()
 
